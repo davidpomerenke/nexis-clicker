eda7c2566dfc8b09ac639da5c69941be9d387079
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2024-02-09 22:48:52 +0100

    Make it work, and adjust to Agence France

--- nexis_scraper/__main__.py
+++ nexis_scraper/__main__.py
@@ -19,13 +19,14 @@ load_dotenv()
 
 # config:
 cookie_path = Path("cookies.json")
-query = "klima*"
+url_path = Path("url.txt")
+query = """(climate) NEAR/10 (protest* OR demo OR rally OR campaign OR "social movement" OR occup* OR strike OR petition OR "hate crime" OR riot OR unrest OR uprising OR boycott OR riot OR activis* OR resistance OR mobilization OR "citizens' initiative" OR march OR parade OR picket OR block* OR sit-in OR vigil OR "hunger strike" OR rebel* OR "civil disobedience")"""
 data_path = Path("data")
 data_path.mkdir(parents=True, exist_ok=True)
 
 
 async def main():
-    await scrape(query, headless=False, backward=True)
+    await scrape(query, headless=False, backward=False)
 
 
 def process_downloads():
@@ -34,16 +35,14 @@ def process_downloads():
 
 
 async def scrape(
-    query=None, headless=True, start=2020, end=2022, backward=False
+    query=None, headless=True, start=2018, end=2023, backward=False
 ) -> pd.DataFrame | None:
     try:
         page, browser, context = await setup(headless=headless)
         page, browser, context = await login(page, browser, context)
-        await page.wait_for_timeout(60_000)
         page, browser, context = await search(
             query, page, browser, context, backward=backward
         )
-        # await page.wait_for_timeout(60_000)
         cookies = await context.cookies()
         cookie_path.write_text(json.dumps(cookies))
         for year in range(start, end + 1):
@@ -91,17 +90,12 @@ async def login(
 ) -> tuple[Page, Browser, BrowserContext]:
     if not cookie_path.exists():
         await page.goto(environ["NEXIS_URL"])
-        await page.wait_for_timeout(2_000)
         await page.fill('input[id="user"]', environ["NEXIS_USER"])
-        await page.wait_for_timeout(2_000)
         await page.fill('input[id="pass"]', environ["NEXIS_PASSWORD"])
-        await page.wait_for_timeout(2_000)
         await click(page, 'input[type="submit"]')
         await page.wait_for_timeout(20_000)
         cookies = await context.cookies()
         cookie_path.write_text(json.dumps(cookies))
-    else:
-        await page.goto(environ["NEXIS_URL"])
     return page, browser, context
 
 
@@ -112,23 +106,34 @@ async def search(
     context: BrowserContext,
     backward: bool = False,
 ) -> tuple[Page, Browser, BrowserContext]:
-    await page.fill("lng-expanding-textarea", query)
-    await click(page, "lng-search-button")
-    try:
-        await click(page, 'span[class="filter-text"]', timeout=15_000)
+    if not url_path.exists():
+        await page.goto(environ["NEXIS_URL"])
+        await page.fill("lng-expanding-textarea", query)
+        await click(page, "lng-search-button")
+        try:
+            await click(page, 'span[class="filter-text"]', timeout=15_000)
+            await page.wait_for_timeout(5_000)
+        except Exception:
+            pass
+        await click(page, 'button[data-filtertype="source"]')
+        await page.wait_for_timeout(1_000)
+        await click(page, 'button[data-action="moreless"]')
         await page.wait_for_timeout(5_000)
-    except Exception:
-        pass
-    await click(page, 'button[data-filtertype="source"]')
-    await page.wait_for_timeout(3_000)
-    el = await page.query_selector('input[data-value="dpa-AFX ProFeed"]')
-    await el.dispatch_event("click")
-    await page.wait_for_timeout(5_000)
-    await click(page, 'span[id="sortbymenulabel"]')
-    await page.wait_for_timeout(1_000)
-    order = "descending" if backward else "ascending"
-    await click(page, f'button[data-value="date{order}"]')
-    await page.wait_for_timeout(5_000)
+        el = await page.query_selector(
+            'input[data-value="Agence France Presse - English"]'
+        )
+        await el.dispatch_event("click")
+        await page.wait_for_timeout(5_000)
+        await click(page, 'span[id="sortbymenulabel"]')
+        await page.wait_for_timeout(1_000)
+        order = "descending" if backward else "ascending"
+        await click(page, f'button[data-value="date{order}"]')
+        await page.wait_for_timeout(5_000)
+        url_path.write_text(page.url)
+    else:
+        url = url_path.read_text()
+        await page.goto(url)
+        await page.wait_for_timeout(5_000)
     return page, browser, context
 
 
@@ -202,19 +207,37 @@ async def download(
         r = range(0, min(n_results - n, n), 100)
     for i in r:
         if not backward:
-            range_ = f"{i+1}-{min(i+100, n_results)}"
+            x, y = i + 1, min(i + 100, n_results)
         if backward:
-            range_ = f"{i+1}-{min(i+100, n_results - 1000)}"
+            x, y = i + 1, min(i + 100, n_results - 1000)
+        range_ = f"{x}-{y}" if x != y else f"{x}"
         b = "B" if backward else ""
         dest_path = data_path / f"zip/{year}-{month:02d}/{b}{range_}.zip"
         dest_path.parent.mkdir(parents=True, exist_ok=True)
         if dest_path.exists():
             continue
-        await page.wait_for_timeout(5_000)
         print(datetime.now().strftime("%H:%M:%S"), "Downloading", dest_path)
         el = await page.query_selector('span[class="icon la-Download"]')
         await el.dispatch_event("click")
-        await page.wait_for_timeout(5_000)
+        await page.wait_for_timeout(2_000)
+        await page.click('a[id="tab-FormattingOptions"]')
+        checkboxes_1 = await page.query_selector_all(
+            'fieldset[class="IncludeOptions"] input[type="checkbox"]'
+        )
+        checkboxes_2 = await page.query_selector_all(
+            'fieldset[class="styling"] input[type="checkbox"]'
+        )
+        for checkbox in checkboxes_1 + checkboxes_2:
+            if await checkbox.bounding_box():
+                await checkbox.set_checked(False)
+                await page.wait_for_timeout(200)
+        await page.click('a[id="tab-BasicOptions"]')
+        await page.wait_for_timeout(500)
+        checkbox = await page.query_selector('input[id="SeparateFiles"]')
+        await checkbox.set_checked(True)
+        checkbox = await page.query_selector('input[id="Rtf"]')
+        await checkbox.set_checked(True)
+        await page.wait_for_timeout(1_000)
         await page.fill('input[id="SelectedRange"]', range_)
         async with page.expect_download(timeout=120_000) as download_info:
             await click(page, 'button[data-action="download"]')
@@ -223,7 +246,8 @@ async def download(
         shutil.move(tmp_path, dest_path)
         print(f"Downloaded {dest_path}")
         process(dest_path)
-        await page.wait_for_timeout(120_000)
+        # await page.wait_for_timeout(120_000)
+        await page.wait_for_timeout(5_000)
     return page, browser, context
 
 
@@ -233,48 +257,36 @@ def unpack(path: Path) -> list[tuple[str, str]]:
         for file in zipObj.filelist:
             if "_doclist" in file.filename:
                 continue
-            rtf = zipObj.read(file).decode("utf-8")
-            plaintext = rtf_to_text(rtf, errors="ignore").strip()
+            rtf = zipObj.read(file).decode(encoding="latin-1")
+            plaintext = rtf_to_text(rtf, encoding="latin-1").strip()
             plaintext = plaintext.replace("\xa0", " ")
             plaintexts.append((file.filename, plaintext))
     return plaintexts
 
 
-def parse(plaintext: str) -> Munch:
+def parse(plaintext: str) -> dict:
     title, rest = plaintext.split("\n", 1)
     feed, rest = rest.split("\n", 1)
-    if feed.startswith("Job Number"):
-        return None
     date, rest = rest.split("\n", 1)
-    date = dateparser.parse(date.strip(), languages=["de"])
-    meta, rest = rest.split("Body", 1)
-    if rest.strip().startswith("Zusammenfassung\n"):
-        _, summary, rest = rest.strip().split("\n", 2)
-        summary = summary.strip()
+    date = dateparser.parse(date.strip(), languages=["en"])
+    location = re.findall(r"Dateline:\s?(.+),[^,]+\n", rest)
+    location = location[0] if len(location) > 0 else None
+    if ", " in location:
+        location, country = location.split(", ")
     else:
-        summary = None
-    try:
-        location, rest = re.split(r" ?\(dpa| ?\(dap", rest, 1)
-        location = location.strip()
-        if rest.startswith("/"):
-            region, rest = re.split(r"\) ?- ?|\) ?– ?", rest[1:], 1)
-            region = region.strip()
-        else:
-            region = ""
-            rest = rest[4:]
-    except ValueError:
-        location = None
-        region = None
+        country = None
+    meta, rest = rest.split("Body", 1)
     if "Graphic" in rest:
+        print("Graph")
         text, _ = rest.split("Graphic", 1)
     else:
         text, _ = rest.split("Load-Date", 1)
     return Munch(
-        title=title.strip(),
-        date=date.isoformat(),
-        summary=summary,
+        date=date.strftime("%Y-%m-%d") if date is not None else None,
+        country=country,
         location=location,
-        region=region,
+        source=feed.strip(),
+        title=title.strip(),
         text=text.strip(),
     )
 
